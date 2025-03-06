d40eff39fdfd508efcea410451f8d0c817df4dba
Ramkumar <rockeyramsaran@gmail.com>
2025-03-07 01:20:26 +0530

    Initial commit: Spring boot project setup

---
+++ Backend/usetiming/src/main/java/com/surya/usetiming/UsetimingApplication.java
@@ -0,0 +1,13 @@
+package com.surya.usetiming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsetimingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsetimingApplication.class, args);
+	}
+
+}
